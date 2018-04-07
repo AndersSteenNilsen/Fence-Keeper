632e68a86a1060c01a9f1067e74f41ccf498e62d
Anders Steen Nilsen <asteennilsen@gmail.com>
2018-04-07 23:03:50 +0200

    initial init

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,8 +6,21 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  items: Array<{ title: string, voltage: string, color: string }>;
 
   constructor(public navCtrl: NavController) {
+    this.items=[];
+    for (let i = 1; i< 11; i++){
+      let tempColor = '';
+      let tempVoltage = Math.random()*10000;
+      if (tempVoltage<3000) tempColor = 'danger';
+      else tempColor = 'primary';
+      this.items.push({
+        title: 'Nede med han...',
+        voltage: tempVoltage,
+        color: tempColor
+      })
+    }
 
   }
 
